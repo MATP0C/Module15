0ed51792278496ce515f66a41cf2a1ca57f3f402
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-07-09 14:06:02 +0300

    15.1.4

--- Module15/Program.cs
+++ Module15/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Linq;
+
+namespace Module15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountCommon("one", "two"));
+        }
+
+        static int CountCommon(string word1, string word2)
+        {
+            var amount = word1.Intersect(word2).Count();
+            return amount;
+        }
+    }
+}
