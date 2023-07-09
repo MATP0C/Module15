72aa00167a97afe2280f0f5453da6deba6e67572
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-07-09 16:41:03 +0300

    15.2.8

--- Module15/Program.cs
+++ Module15/Program.cs
@@ -5,23 +5,40 @@ namespace Module15
 {
     class Program
     {
+        //   статическая переменная для хранения данных в памяти
+        public static List<int> Numbers = new List<int>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите текст");
-            var text = Console.ReadLine();
-
-            var punctuation = new List<char>() { ' ', ',', '.', ';', ':', '!', '?' };
-
-            if (string.IsNullOrEmpty(text))
+            while (true)
             {
-                Console.WriteLine("Вы ввели пустой текст");
-                return;
-            }
-            Console.WriteLine();
-            Console.WriteLine("Текст без знаков препинания: ");
+                // Читаем введенный с консоли  текст
+                var input = Console.ReadLine();
 
-            var noPunctuation = text.Except(punctuation).ToArray();
-            Console.WriteLine(noPunctuation);
-        } 
+                // проверяем, число ли это
+                var isInteger = Int32.TryParse(input, out int inputNum);
+
+                // если не число - показываем ошибку
+                if (!isInteger)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Вы ввели не число");
+                }
+                // если соответствует, запускаем обработчик
+                else
+                {
+                    // добавляем в список
+                    Numbers.Add(inputNum);
+
+                    // выводим все критерии
+                    Console.WriteLine("Число " + input + " добавлено в список.");
+                    Console.WriteLine($"&quot; Всего в списке { Numbers.Count} чисел");
+                    Console.WriteLine($"Сумма:  {Numbers.Sum()}");
+                    Console.WriteLine($"Наибольшее:  {Numbers.Max()}");
+                    Console.WriteLine($"Наименьшее:  {Numbers.Min()}"); Console.WriteLine($"Среднее:  {Numbers.Average()}");
+                    Console.WriteLine();
+                }
+            }
+        }
     }
 }
