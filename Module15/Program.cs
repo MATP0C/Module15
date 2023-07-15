2f894a0e41b18f26f40efab0f5e3e01d711db656
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-07-15 12:37:07 +0300

    15.6.6. Практическое задание (HW-03)

--- Module15/Program.cs
+++ Module15/Program.cs
@@ -1,44 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Module15
 {
     class Program
     {
-        //   статическая переменная для хранения данных в памяти
-        public static List<int> Numbers = new List<int>();
-
         static void Main(string[] args)
         {
-            while (true)
+            var classes = new Classroom[]
             {
-                // Читаем введенный с консоли  текст
-                var input = Console.ReadLine();
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            string allStudents = GetAllStudents(classes);
 
-                // проверяем, число ли это
-                var isInteger = Int32.TryParse(input, out int inputNum);
-
-                // если не число - показываем ошибку
-                if (!isInteger)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Вы ввели не число");
-                }
-                // если соответствует, запускаем обработчик
-                else
-                {
-                    // добавляем в список
-                    Numbers.Add(inputNum);
-
-                    // выводим все критерии
-                    Console.WriteLine("Число " + input + " добавлено в список.");
-                    Console.WriteLine($"&quot; Всего в списке { Numbers.Count} чисел");
-                    Console.WriteLine($"Сумма:  {Numbers.Sum()}");
-                    Console.WriteLine($"Наибольшее:  {Numbers.Max()}");
-                    Console.WriteLine($"Наименьшее:  {Numbers.Min()}"); Console.WriteLine($"Среднее:  {Numbers.Average()}");
-                    Console.WriteLine();
-                }
-            }
+            Console.WriteLine("Все студенты в текстовом формате: ");
+            Console.WriteLine(allStudents);
         }
+
+        private static string GetAllStudents(Classroom[] classes)
+        {
+            return string.Join(" ", classes.SelectMany(c => c.Students));
+        }
+    }
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
     }
 }
